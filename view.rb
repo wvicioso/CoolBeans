297d2f325406f8939164581a14384283aba9f1b3
regulus33 <zacharyrowden89@gmail.com>
2016-09-30 14:34:20 -0400

    add ascii art

--- view.rb
+++ view.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 module View
 
 	def self.get_zip
@@ -15,8 +17,41 @@ module View
 	end
 
 	def self.welcome
-		puts "Welcome to Cool Beans!"
-	end
+		welcome = <<-WELCOME 
+ _     _  _______  ___      _______  _______  __   __  _______   _______  _______ 
+| | _ | ||       ||   |    |       ||       ||  |_|  ||       | |       ||       |
+| || || ||    ___||   |    |       ||   _   ||       ||    ___| |_     _||   _   |
+|       ||   |___ |   |    |       ||  | |  ||       ||   |___    |   |  |  | |  |
+|       ||    ___||   |___ |      _||  |_|  ||       ||    ___|   |   |  |  |_|  |
+|   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___    |   |  |       |
+|__| |__||_______||_______||_______||_______||_|   |_||_______|   |___|  |_______|
+
+ _______  _______  _______  ___       _______  _______  _______  __    _  _______ 
+|       ||       ||       ||   |     |  _    ||       ||   _   ||  |  | ||       |
+|       ||   _   ||   _   ||   |     | |_|   ||    ___||  |_|  ||   |_| ||____   |
+|       ||  | |  ||  | |  ||   |     |       ||   |___ |       ||       | ____|  |
+|      _||  |_|  ||  |_|  ||   |___  |  _   | |    ___||       ||  _    || ______|
+|     |_ |       ||       ||       | | |_|   ||   |___ |   _   || | |   || |_____ 
+|_______||_______||_______||_______| |_______||_______||__| |__||_|  |__||_______|
+
+WELCOME
+
+  3.times do 
+    puts welcome.red
+    puts "What's your zip?"
+    sleep(0.3)
+    clear_screen
+    puts welcome.white
+    puts "What's your zip?"
+    sleep(0.3)
+    clear_screen
+    puts welcome.green
+    puts "What's your zip?"
+    sleep(0.3)
+    clear_screen
+  end
+    puts welcome.green
+  end
 
   def self.no_rest
     puts "Sorry, no Mexican spots around you!"
